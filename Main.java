11899bad3c4e9d290f46b6fad9516518fa7d288d
Edu Caro <eduardo.caro.lorente@gmail.com>
2023-12-12 09:25:50 +0100

    Ejercicio 3 añadido

--- Main.java
+++ Main.java
@@ -5,23 +5,25 @@ public class Main {
         // Ejercicio 1
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce un número: ");
-        int numero1 = teclado.nextInt();
+            int numero1 = teclado.nextInt();
         System.out.println("Introduce otro número");
-        int numero2 = teclado.nextInt();
-        int suma = numero1+numero2;
-        int resta = numero1-numero2;
-        int multi = numero1 * numero2;
-        float division = (float) numero1 / numero2;
+            int numero2 =  teclado.nextInt();
+            int suma =  numero1+numero2;
+            int resta = numero1-numero2;
+            int multi = numero1 * numero2;
+            double division =  (double) numero1 / numero2;
+            int potencia = (int) Math.pow(numero1, numero2);
         System.out.println("Suma: "+suma);
         System.out.println("Resta: "+resta);
         System.out.println("Multiplicación: "+multi);
         System.out.println("División: "+division);
+        System.out.println("Potencia: "+potencia);
 
         // Ejercicio 2
         System.out.println("Escribe otro valor: ");
-        int numero3 = teclado.nextInt();
+            int numero3 = teclado.nextInt();
         System.out.println("Uno más");
-        int numero4 = teclado.nextInt();
+            int numero4 = teclado.nextInt();
         if(numero3 > numero4){
             System.out.println("El mayor es "+numero3);
         }
@@ -30,5 +32,29 @@ public class Main {
         }
         else {
             System.out.println("Son iguales");}
+
+        // Ejercicio 3
+        System.out.println("Introduce el precio del producto:");
+            float precio = teclado.nextFloat();
+        System.out.println("Introduce el % de IVA (4%, 10%, 21%):");
+            int iva = teclado.nextInt();
+        double total;
+            switch(iva){
+                case 4:
+                    total = precio * 1.04;
+                    System.out.println("Total: "+total);
+                    break;
+                case 10:
+                    total = precio * 1.1;
+                    System.out.println("Total: "+total);
+                    break;
+                case 21:
+                    total = precio * 1.21;
+                    System.out.println("Total: "+total);
+                    break;
+                default:
+                    System.out.println("Valor de iva incorrecto");
+            }
         }
+
     }
